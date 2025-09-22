83f6d37164c4f917b8d8c3050b52006961c60751
kishorkumar-b <kishorkumar.b@agaramtech.com>
2025-09-22 19:02:16 +0530

    22/09/2025

---
+++ sample.js
@@ -0,0 +1,33 @@
+const car=["mg",'kia',"bmw",]
+car.push('mg')
+console.log(car.includes("kia"))
+console.log(typeof(car))
+console.log(car.length)
+console.log(car.sort())
+console.log(car.toString())
+console.log(typeof null);
+
+console.log(car.at(3))
+for(i=car.length-1;i>=0;i--){
+    console.log(car[i])
+}
+
+console.log(car.join(" "))
+car.pop()
+console.log(car)
+console.log(car.shift())
+console.log(car.flat())
+
+
+let position = car.indexOf("bmw") + 1;
+console.log(position);
+let lastposition = car.lastIndexOf("bmw") + 1;
+console.log(lastposition);
+
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+console.log(first)
